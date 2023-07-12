2e6c5750ae08345898e7f5fc96fc9df5c24301ca
Daniel-Digid <daniel@dig.id>
2023-07-12 14:34:40 +0100

    dev

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,10 +1,40 @@
-<section class="section-intro py-16 xl:py-32 bg-bg-color overflow-hidden">
-	<div class="lg:container mx-auto px-8 grid grid-cols-1 md:grid-cols-12">
-		<div class="col-span-1 md:col-span-5 lg:col-span-4">
-			<h2 class="title-normal !font-normal"><?php the_field( 'intro_title' ); ?></h2>
+<section class="section-intro py-8 xl:py-32 bg-bg-color overflow-hidden">
+	<div class="container mx-auto px-8 grid grid-cols-1 md:grid-cols-12">
+		<div class="col col-span-1 lg:col-span-3 lg:col-start-7">
+			<h2 class="title-normal"><?php the_field( 'section_intro_head_title' ); ?></h2>
 		</div>
-		<div class="col col-span-1 md:col-span-6 lg:col-span-5 md:col-start-7 lg:col-start-6">
-			<p class="text-body"><?php the_field( 'intro_description' ); ?></p>
+	</div>
+	<div class="container mx-auto pb-16 px-8 grid grid-cols-1 md:grid-cols-12">
+		<div class="col col-span-1 lg:col-span-6">
+			<a href="" class="" type="button"></a>
+		</div>
+		<div class="col col-span-1 lg:col-span-6">
+			<h2 class="text-body columns-2"><?php the_field( 'section_intro_head_text' ); ?></h2>
+		</div>
+	</div>
+	<div class="container mx-auto py-16 px-8 grid grid-cols-1 md:grid-cols-9">
+		<div class="col col-span-1 lg:col-span-1 lg:col-start-5 justify-center flex">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/dot_sep.svg">
+		</div>
+		<div class="col col-span-1 lg:col-span-7 lg:col-start-2 justify-center flex">
+		<?php 
+			$intro_image = get_field('section_intro_head_image');
+			if( $intro_image ) {
+				echo wp_get_attachment_image( $intro_image, 'full' );
+			} 
+		?>
+		</div>
+		<div class="col col-span-1 lg:col-span-1 lg:col-start-5 justify-center flex">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/dot_sep.svg">
+		</div>
+	</div>
+
+	<div class="container mx-auto py-16 px-8 grid grid-cols-1 md:grid-cols-12">
+		<div class="col col-span-1 lg:col-span-12 flex justify-center pb-8">
+			<h2 class="title-normal"><?php the_field( 'section_intro_foot_title' ); ?></h2>
+		</div>
+		<div class="col col-span-1 lg:col-span-8 lg:col-start-3 flex justify-center">
+			<h2 class="text-body columns-2"><?php the_field( 'section_intro_foot_text' ); ?></h2>
 		</div>
 	</div>
 </section>
