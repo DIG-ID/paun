0a8279ebf5d898df974002d22cdac04d391425d8
Daniel-Digid <daniel@dig.id>
2023-07-14 16:57:28 +0100

    dev

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<?php get_template_part( 'template-parts/footer', 'main' ); ?>
 		<?php wp_footer(); ?>
 		<?php if (is_page('angebote')) : ?>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+			<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/flowbite.min.js"></script>
 		<?php endif; ?>
 	</body>
 
