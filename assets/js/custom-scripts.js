d33bfe6b6baf29b4e40439d3513c9d5ff55d89f2
Daniel-Digid <daniel@dig.id>
2023-07-18 13:12:53 +0100

    dev

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,19 +1,27 @@
 jQuery(document).ready(function($) {
-    $(window).scroll(function() {
+    function debounce(func, delay) {
+        let timer;
+        return function() {
+          clearTimeout(timer);
+          timer = setTimeout(func, delay);
+        };
+      }
+      
+      $(window).scroll(debounce(function() {
         var header = $('#header-main');
         var logo = $('.header-logo');
         var scrollTop = $(window).scrollTop();
-        var threshold = 300; // Adjust the value (in pixels) as needed
+        var threshold = 0; // Adjust the value (in pixels) as needed
         var isSticky = header.hasClass('sticky');
-    
+      
         if (scrollTop > threshold && !isSticky) {
-            header.addClass('sticky');
-            logo.addClass('sticky-logo');
-            logo.removeClass('transition-logo-width');
+          header.addClass('sticky');
+          logo.addClass('sticky-logo');
+          logo.removeClass('transition-logo-width');
         } else if (scrollTop <= threshold && isSticky) {
-            header.removeClass('sticky');
-            logo.removeClass('sticky-logo');
-            logo.addClass('transition-logo-width');
+          header.removeClass('sticky');
+          logo.removeClass('sticky-logo');
+          logo.addClass('transition-logo-width');
         }
-    });
+      }, 50)); // Adjust the debounce delay (in milliseconds) as needed
 });
