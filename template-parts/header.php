e84c5abe4e2ae3b205f9975d810d5cfe70026d47
Daniel-Digid <daniel@dig.id>
2023-07-25 17:22:18 +0100

    dev

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,6 +3,20 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+		<link
+		rel="icon"
+		type="image/png"
+		sizes="32x32"
+		href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico"
+		media="(prefers-color-scheme: dark)"
+		/>
+		<link
+		rel="icon"
+		type="image/png"
+		sizes="32x32"
+		href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon_dark.ico"
+		media="(prefers-color-scheme: light)"
+		/>
 		<?php wp_head(); ?>
 	</head>
 
