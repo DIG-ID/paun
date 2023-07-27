33796899a3402934d6252bdf54b599d0cbd334fb
Daniel-Digid <daniel@dig.id>
2023-07-27 11:59:11 +0100

    dev

--- template-parts/pages/page/page-content.php
+++ template-parts/pages/page/page-content.php
@@ -1,5 +1,5 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'overflow-hidden py-8 lg:py-20' ); ?>>
-	<div class="container mx-auto px-8 grid grid-cols-1 lg:grid-cols-12">
+	<div class="container mx-auto px-8 xl:px-0 grid grid-cols-1 lg:grid-cols-12">
 		<div class="post-content-wrapper col-span-8">
 			<h1 class="title-normal"><?php the_title(); ?></h1>
 			<?php
