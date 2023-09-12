46e752a57516d50572362768e126d1512528dc09
Daniel-Digid <daniel@dig.id>
2023-09-12 14:18:02 +0100

    d

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -5,7 +5,6 @@
 
 get_header();
 	do_action( 'before_main_content' );
-		do_action( 'breadcrumbs' );
 		get_template_part( 'template-parts/pages/contact/intro' );
 		get_template_part( 'template-parts/pages/contact/contacts' );
 		get_template_part( 'template-parts/pages/contact/map' );
