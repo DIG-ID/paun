9755d1f050d61b4fe58a5ee2e5bacbefb96cd43b
Daniel-Digid <daniel@dig.id>
2023-11-08 17:04:08 +0000

    d

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,18 +1,18 @@
-<section class="section-intro py-8 lg:pb-32 lg:pt-16 bg-bg-color overflow-hidden">
-	<div class="container mx-auto px-8 grid grid-cols-2 sm:grid-cols-7 lg:grid-cols-12">
+<section class="section-intro pt-8 pb-0 md:py-8 lg:pb-32 lg:pt-16 bg-bg-color overflow-hidden">
+	<div class="container mx-auto px-8 grid grid-cols-2 sm:grid-cols-7 lg:grid-cols-12 pt-5 md:pt-0">
 		<div class="col col-span-2 sm:col-span-3 lg:col-span-3 lg:col-start-7">
-			<h2 class="title-normal"><?php the_field( 'section_intro_head_title' ); ?></h2>
+			<h2 class="title-normal !mb-5 md:!mb-8"><?php the_field( 'section_intro_head_title' ); ?></h2>
 		</div>
 	</div>
-	<div class="container mx-auto pb-16 px-8 grid grid-cols-1 lg:grid-cols-12">
-		<div class="col col-span-2 sm:col-span-1 lg:col-span-6 pt-2 order-2 lg:order-1 flex lg:block justify-end">
+	<div class="container mx-auto pb-7 md:pb-16 px-8 grid grid-cols-1 lg:grid-cols-12">
+		<div class="col col-span-2 sm:col-span-1 lg:col-span-6 pt-2 order-2 lg:order-1 flex lg:block justify-start md:justify-end">
 			<a class="btn-order btn-disabled px-8 py-2" type="button"><?php esc_html_e( 'Jetzt bestellen', 'paun' ) ?><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow-more.svg" class="mx-4 hidden lg:block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow_mob.svg" class="mr-0 ml-4 lg:hidden block"></a>
 		</div>
 		<div class="col-span-1 lg:col-span-6 order-1 lg:order-2">
 			<h2 class="text-body sm:columns-2"><?php the_field( 'section_intro_head_text' ); ?></h2>
 		</div>
 	</div>
-	<div class="container mx-auto pt-0 pb-10 sm:pt-16 sm:pb-16 md:px-8 grid grid-cols-1 lg:grid-cols-9">
+	<div class="container mx-auto pt-0 pb-0 md:pb-10 sm:pt-16 sm:pb-16 md:px-8 grid grid-cols-1 lg:grid-cols-9">
 		<div class="col col-span-1 lg:col-span-1 lg:col-start-5 justify-center flex">
 			<img class="dot_sep" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/dot_sep.svg">
 		</div>
@@ -25,12 +25,12 @@
 		</div>
 	</div>
 
-	<div class="container mx-auto py-16 px-8 grid grid-cols-1 lg:grid-cols-12">
+	<div class="container mx-auto pt-5 pb-[68px] md:py-16 px-8 grid grid-cols-1 lg:grid-cols-12">
 		<div class="col col-span-1 lg:col-span-12 flex justify-center pb-8">
-			<h2 class="title-normal"><?php the_field( 'section_intro_foot_title' ); ?></h2>
+			<h2 class="title-normal !mb-0 md:!mb-8"><?php the_field( 'section_intro_foot_title' ); ?></h2>
 		</div>
 		<div class="col col-span-1 lg:col-span-8 lg:col-start-3 flex justify-center">
-			<h2 class="text-body sm:columns-2"><?php the_field( 'section_intro_foot_text' ); ?></h2>
+			<h2 class="text-body !mb-0 md:!mb-8 sm:columns-2"><?php the_field( 'section_intro_foot_text' ); ?></h2>
 		</div>
 	</div>
 </section>
--- template-parts/pages/home/popup-store.php
+++ template-parts/pages/home/popup-store.php
@@ -1,31 +1,31 @@
-<section class="section-popup-store pb-32 lg:pb-44 bg-bg-color overflow-hidden w-full sm:w-11/12 mx-auto px-8 sm:px-0">
+<section class="section-popup-store pb-[72px] md:pb-32 lg:pb-44 bg-bg-color overflow-hidden w-full sm:w-11/12 mx-auto px-8 sm:px-0">
     <div class="container mx-auto grid grid-cols-1 lg:grid-cols-12 pt-8 border-t-2 border-text-color">
 		<div class="col-span-1 lg:col-span-12">
-			<h2 class="title-section"><?php the_field( 'section_popupstore_section_title' ); ?></h2>
+			<h2 class="title-section !mb-5 md:!mb-8"><?php the_field( 'section_popupstore_section_title' ); ?></h2>
 		</div>
 	</div>
 
-    <div class="container mx-auto grid grid-cols-5 sm:grid-cols-2 lg:grid-cols-9 pt-3 lg:pt-16">
+    <div class="container mx-auto grid grid-cols-5 sm:grid-cols-2 lg:grid-cols-9 md:pt-3 lg:pt-16">
 		<div class="col-span-3 lg:col-span-3">
-			<h3 class="title-normal"><?php the_field( 'section_popupstore_title' ); ?></h3>
+			<h3 class="title-normal !mb-16 md:!mb-8"><?php the_field( 'section_popupstore_title' ); ?></h3>
 		</div>
 	</div>
 
-    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-9 pt-4">
+    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-9 md:pt-4">
 		<div class="col-span-1 sm:col-span-1 lg:col-span-3 grid grid-cols-4">
-            <div class="col-span-1 pr-3 pt-5">
+            <div class="col-span-1 pr-3 pt-7 md:pt-5">
             	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow-notarrow.svg">
 			</div>
 			<div class="col-span-3">
-            	<p class="text-bigger"><?php the_field( 'section_popupstore_schedule' ); ?></p>
+            	<p class="text-bigger !leading-7 max-w-xs md:max-w-none"><?php the_field( 'section_popupstore_schedule' ); ?></p>
 			</div>
 		</div>
-        <div class="col-span-1 sm:col-span-1 lg:col-span-3 pt-8 sm:pt-0 sm:px-8">
+        <div class="col-span-1 sm:col-span-1 lg:col-span-3 pt-9 sm:pt-0 sm:px-8">
 			<p class="text-body"><?php the_field( 'section_popupstore_description' ); ?></p>
 		</div>
-        <div class="col-span-1 sm:col-span-2 lg:col-span-3 pt-5 sm:pt-20 lg:pt-0 sm:px-8">
+        <div class="col-span-1 sm:col-span-2 lg:col-span-3 pt-0 sm:pt-20 lg:pt-0 sm:px-8">
 			<p class="text-bigger mb-8 sm:mb-20 w-full sm:w-1/2 lg:w-full float-left lg:float-none"><?php the_field( 'section_popupstore_order_text' ); ?></p>
-            <a class="btn-order btn-disabled px-8 py-2 float-right sm:float-none" type="button"><?php esc_html_e( 'Coming soon', 'paun' ) ?><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow-more.svg" class="mx-4 hidden lg:block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow_mob.svg" class="mr-0 ml-4 lg:hidden block"></a>
+            <a class="btn-order btn-disabled px-8 py-2" type="button"><?php esc_html_e( 'Coming soon', 'paun' ) ?><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow-more.svg" class="mx-4 hidden lg:block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Arrow_mob.svg" class="mr-0 ml-4 lg:hidden block"></a>
 		</div>
 	</div>
 </section>
